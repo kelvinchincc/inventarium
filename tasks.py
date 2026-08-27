14d4bbdf3f8dfdad7bfe71229afde5ad02527ffc
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-08-28 00:32:44 +0800

    Refactor tasks.py to use invoke.tasks and improve development task
    Imports invoke context instead of Typer for the dev task and adjust execution flow

--- tasks.py
+++ tasks.py
@@ -1,27 +1,14 @@
-# /// script
-# requires-python = ">=3.14"
-# dependencies = [
-#     "typer",
-# ]
-# ///
-
 import subprocess
 
-from typer import Typer
+from invoke.context import Context
+from invoke.tasks import task  # pyright: ignore[reportUnknownVariableType]
 
-app = Typer()
 
-
-@app.command()
-def dev():
+@task
+def dev(_c: Context):
     """
     Run the dev server with bacon
 
     Replace current process with bacon, `bacon run -- cargo run`
     """
-
     _ = subprocess.run(["bacon", "run", "--", "cargo", "run"], check=True)
-
-
-if __name__ == "__main__":
-    app()

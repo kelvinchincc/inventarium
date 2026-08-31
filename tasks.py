06215f1f1b23a5f91770aa1c9db68c10771a37e0
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-09-01 01:08:12 +0800

    Switch dev task to use watchdog for automatic server restarts

--- tasks.py
+++ tasks.py
@@ -7,8 +7,22 @@ from invoke.tasks import task  # pyright: ignore[reportUnknownVariableType]
 @task
 def dev(_c: Context):
     """
-    Run the dev server with bacon
+    Run the dev server with watchdog
 
-    Replace current process with bacon, `bacon run -- cargo run`
+    Replace current process with watchmedo auto-restart, which will watch for changes in the source code and restart the
+    server automatically. Watchdog must be installed first via pip or uv tool, eg: `uv tool install watchdog`. This is a
+    workaround for the fact that bacon and watchexec-cli having issues with file watching on Windows. See
+    https://github.com/Canop/bacon/issues/62
     """
-    _ = subprocess.run(["bacon", "run", "--", "cargo", "run"], check=True)
+
+    # fmt: off
+    _ = subprocess.run([
+        "watchmedo",
+        "auto-restart",
+        "-d", ".",
+        "-p", "**/*.rs;**/*.sql;**/*.toml",
+        "--recursive",
+        "--",
+        "cargo", "run"
+    ], check=True)
+    # fmt: on
